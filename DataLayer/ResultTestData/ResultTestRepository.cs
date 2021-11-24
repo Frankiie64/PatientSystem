196177dc009c2aaa4d2fe08ad8395dd8d570fdcc
Franklyn Brea <franklynbrea38@gmail.com>
2021-11-24 17:21:55 -0500

    Doing changes 'retoques'

--- DataLayer/ResultTestData/ResultTestRepository.cs
+++ DataLayer/ResultTestData/ResultTestRepository.cs
@@ -16,7 +16,7 @@ namespace DataLayer.ResultTestData
         }
         public DataTable GetAll()
         {
-            SqlCommand cmd = new SqlCommand("select Patients.FName as 'Name', Patients.LastName as 'Last Name', Patients.identification,LabTest.Title from LabResult inner join Patients ON id_Patients = Patients.id INNER JOIN LabTest ON id_LabTest = LabTest.id", _cn);
+            SqlCommand cmd = new SqlCommand("select LabResult.Id Patients.FName as 'Name', Patients.LastName as 'Last Name', Patients.identification,LabTest.Title from LabResult inner join Patients ON id_Patients = Patients.id INNER JOIN LabTest ON id_LabTest = LabTest.id", _cn);
             cmd.CommandType = CommandType.Text;
 
             SqlDataAdapter query = new SqlDataAdapter(cmd);
@@ -63,6 +63,48 @@ namespace DataLayer.ResultTestData
             catch (Exception ex)
             {
                 return null;
+            }
+
+        }
+        public bool EditApt(int id)
+        {
+            SqlCommand cmd = new SqlCommand("update Appointment set StatusAppointment =+ 1 where Id = @id", _cn);
+            cmd.CommandType = CommandType.Text;
+
+            cmd.Parameters.AddWithValue("@id", id);
+
+            return ExecuteDml(cmd);
+        }
+        public bool edit(int id,string StatusResult)
+        {
+            SqlCommand cmd = new SqlCommand("update LabResult set StatusResult = @StatusResult where Id = @id;", _cn);
+            cmd.CommandType = CommandType.Text;
+
+            cmd.Parameters.AddWithValue("@StatusResult", StatusResult);
+            cmd.Parameters.AddWithValue("@id", id);
+
+            return ExecuteDml(cmd);
+        }
+        private bool ExecuteDml(SqlCommand command)
+        {
+            try
+            {
+                _cn.Close();
+                _cn.Open();
+
+                command.ExecuteNonQuery();
+
+                _cn.Close();
+
+                return true;
+
+            }
+            catch (Exception ex)
+            {
+
+                return false;
+
+
             }
 
         }
---
+++ LogicLayer/LogicTestResult/ServiceTestResult.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using DataLayer.ResultTestData;
+
+namespace LogicLayer.LogicTestResult
+{
+    public class ServiceTestResult
+    {
+        SqlConnection _cn;
+        ResultTestRepository _repo;
+
+        ServiceTestResult(SqlConnection cn)
+        {
+            _cn = cn;
+            _repo = new ResultTestRepository(_cn);
+        }
+
+        public bool ChangesApoitment(int id)
+        {                        
+            return _repo.EditApt(id);                                   
+        }
+
+        private bool AddResult(int id, string Result)
+        {
+            return _repo.edit(id, Result);
+        }
+    }
+}
